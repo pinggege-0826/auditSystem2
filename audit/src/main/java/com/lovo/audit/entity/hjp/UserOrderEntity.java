3539f3720604a7f12504ef0ead271a5b4d9d25c0
pinggege-0826 <1245752108@qq.com>
2020-01-13 17:24:12 +0800

    审核系统

--- audit/src/main/java/com/lovo/audit/entity/hjp/UserOrderEntity.java
+++ audit/src/main/java/com/lovo/audit/entity/hjp/UserOrderEntity.java
@@ -1,14 +1,42 @@
 package com.lovo.audit.entity.hjp;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "sys_userOrder")
 public class UserOrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "u_orderId")
+    @Column(name = "uo_orderId")
     private int id;
+    /**前端用户ID*/
+    @Column(name = "uo_user_Id")
+    private String userId;
+
+    /**订单编号*/
+    @Column(name = "uo_orderNumx")
+    private String orderCode;
+
+    /**下单时间*/
+    @Column(name = "uo_orderTime")
+    private String orderTime;
+
+    /**购买资金来源*/
+    @Column(name = "uo_moneySource")
+    private String moneySource;
+
+    /**总金额*/
+    @Column(name = "uo_allMoney")
+    private double allMoney;
+
+    /**结算状态*/
+    @Column(name = "u_tag")
+    private int tag;
+
+    /**商品集合*/
+    @OneToMany(mappedBy = "userOrderEntity")
+    private List<UserGoodsEntity> goodsList;
 
 }
 
