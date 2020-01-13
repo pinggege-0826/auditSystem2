ac3f25dfbb5e349d79e2d6a0821c0525b6fc88bc
lh2123 <1572105496@qq.com>
2020-01-13 17:02:10 +0800

    审核系统2

---
+++ audit/src/main/java/com/lovo/audit/entity/lh/MarketingEntity.java
@@ -0,0 +1,113 @@
+package com.lovo.audit.entity.lh;
+
+import javax.persistence.*;
+
+/**
+ * 促销审核实体类
+ */
+@Entity
+@Table(name = "sys_marketing")
+public class MarketingEntity {
+    /**
+     * 商品ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+    /**
+     * 商品编号
+     */
+    @Column(length = 80)
+    private String productCode;
+    /**
+     * 商品名
+     */
+    @Column(length = 80)
+    private String productName;
+    /**
+     * 商品单价
+     */
+    private double productPrice;
+    /**
+     * 商品规格
+     */
+    @Column(length = 80)
+    private String productNorms;
+    /**
+     * 商品类型
+     */
+    @Column(length = 48)
+    private String productType;
+    /**
+     * 促销折扣
+     */
+    private double discount;
+    /**
+     * 促销方案状态，0：未通过  1：已通过  2：已审核
+     */
+    private int status;
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductNorms() {
+        return productNorms;
+    }
+
+    public void setProductNorms(String productNorms) {
+        this.productNorms = productNorms;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
