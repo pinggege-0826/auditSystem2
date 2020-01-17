2a4c3834e5de0ced188dc6424ea631445ec04ac4
Lenovo <871066793@qq.com>
2020-01-17 13:14:01 +0800

    copy

--- audit/src/main/java/com/lovo/audit/dao/cpy/ICompanyDao.java
+++ audit/src/main/java/com/lovo/audit/dao/cpy/ICompanyDao.java
@@ -1,11 +1,10 @@
 package com.lovo.audit.dao.cpy;
 
 import com.lovo.audit.entity.cpy.CompanyEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface ICompanyDao extends CrudRepository<CompanyEntity,Integer> {
@@ -17,12 +16,12 @@ public interface ICompanyDao extends CrudRepository<CompanyEntity,Integer> {
     public List<CompanyEntity> findAllCompany(Pageable pageable);
     /**
      * 通过公司ID修改公司状态
-     * @param status 公司状态
+     * @param level 公司状态
      * @param id 公司编号
      */
-    @Query("update CompanyEntity set companyStatus = ?1 where id = ?2")
+    @Query("update CompanyEntity set companyStatus ='1',companyLevel =?1 where id = ?2")
     @Modifying
-    public void updateStatusById(String status, Integer id);
+    public void updateStatusById(String level, Integer id);
     /**
      * 通过公司ID查询公司
      * @param id 公司ID
@@ -30,4 +29,17 @@ public interface ICompanyDao extends CrudRepository<CompanyEntity,Integer> {
      */
     @Query("from CompanyEntity where id = ?1")
     public CompanyEntity findCompanyById(Integer id);
+
+    /**
+     * 删除供应商
+     * @param id 公司id
+     */
+    @Query("delete from CompanyEntity where id=?1")
+    @Modifying
+    public void deleteCompany(Integer id);
+    /**
+     * 统计总页数
+     * @return  总页数
+     */
+    public int countAllBy();
 }
--- audit/src/main/java/com/lovo/audit/service/cpy/ICompanyService.java
+++ audit/src/main/java/com/lovo/audit/service/cpy/ICompanyService.java
@@ -2,11 +2,46 @@ package com.lovo.audit.service.cpy;
 
 
 import com.lovo.audit.entity.cpy.CompanyEntity;
+import com.lovo.audit.entity.lh.MarketingEntity;
+
+import java.util.List;
 
 public interface ICompanyService {
     /**
-     * 添加电脑
-     * @param company 电脑对象
+     * 添加公司
+     * @param company 供应商对象
      */
     public void addCompany(CompanyEntity company);
+    /**
+     * 查询所有供应商并分页展示
+     * @param currPage  当前页
+     * @param pageSize  每页显示条数
+     * @return          分好页的供应商集合
+     */
+    public List<CompanyEntity> findAllCompany(int currPage,int pageSize);
+
+    /**
+     * 根据公司Id查询详情
+     * @param id  公司ID
+     * @return    供应商对象
+     */
+    public CompanyEntity findCompanyById(Integer id);
+
+    /**
+     * 根据公司Id修改供应商的状态
+     * @param id       公司Id
+     * @param level   供应商状态  （0为未审核，1为正常，2为冻结）
+     */
+    public void updateStatusById(String level,Integer id);
+
+    /**
+     * 删除供应商
+     * @param id 公司id
+     */
+    public void deleteCompany(Integer id);
+    /**
+     * 统计总页数
+     * @return  总页数
+     */
+    public int countAllBy();
 }
