cfde4c39f17d75dc102f994df5c0de12084e78fd
Lenovo <871066793@qq.com>
2020-02-15 15:13:46 +0800

    copy0215

--- audit/src/main/java/com/lovo/audit/websocket/WebSocketServer.java
+++ audit/src/main/java/com/lovo/audit/websocket/WebSocketServer.java
@@ -1,7 +1,11 @@
 package com.lovo.audit.websocket;
 
 
+import com.lovo.audit.entity.cpy.CompanyEntity;
+import com.lovo.audit.service.cpy.ICompanyService;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
@@ -17,6 +21,8 @@ public class WebSocketServer {
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
+
+    private ICompanyService companyService;
 
     /**
      * 连接建立成功调用的方法*/
@@ -62,6 +68,20 @@ public class WebSocketServer {
     @RabbitListener(queues = "getSalesPassFromCheckQueue")
     public  void getMessage(String message) throws IOException {
         System.out.println(message);
+        if (sessions.size() != 0) {
+            for (WebSocketServer s : sessions) {
+                if (s != null) {
+                    s.session.getBasicRemote().sendText("cuXiao");
+                }
+            }
+        }
+    }
+    @RabbitListener(queues = "getDataQueue")
+    public  void companyMessage(String message) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        CompanyEntity company = mapper.readValue(message,CompanyEntity.class);
+        companyService.addCompany(company);
+        System.out.println(company);
         if (sessions.size() != 0) {
             for (WebSocketServer s : sessions) {
                 if (s != null) {
